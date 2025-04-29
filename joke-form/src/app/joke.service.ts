bc91fb810714b0ba38c7622a31f89e80ec970e1f
Katinka Hesselink <29568495+smilingkite@users.noreply.github.com>
2025-04-29 11:07:14 +0200

    use httpClient in jokeService

--- joke-form/src/app/joke.service.ts
+++ joke-form/src/app/joke.service.ts
@@ -1,22 +1,23 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { catchError, of, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class JokeService {
-  fetchJokes(): Observable<string> {
-    return new Observable<string>((observer) => {
-      fetch('https://api.chucknorris.io/jokes/random')
-        .then((response) => response.json())
-        .then((data) => {
-          observer.next(data.value);
-          observer.complete();
-        })
-        .catch((error) => {
+  private http = inject(HttpClient);
+  fetchJokes() {
+    return this.http
+      .get<{ value: string }>('https://api.chucknorris.io/jokes/random')
+      .pipe(
+        switchMap((data) => {
+          return of(data.value);
+        }),
+        catchError((error) => {
           console.error('Error fetching joke:', error);
-          observer.error(error);
-        });
-    });
+          return of(`Error: ${error.message}`);
+        })
+      );
   }
 }
